589261363983b5bf7009e5a67a9135b67d9d0b62
Maciek <sadkowskim@yahoo.pl>
2023-05-24 16:44:43 +0200

    added UK data, implemented data extraction

---
+++ src/Tidy/data_extracter.r
@@ -0,0 +1,16 @@
+
+
+load_datasets <- function(country){
+  path_age_sex_population <- paste0("Data/",country, "/", country,"_age_sex_population.csv")
+  age_sex_population <- read_csv(path_age_sex_population)
+  
+  path_region_age_sex_population <- paste0("Data/",country, "/", country,"_region_age_sex_population.csv")
+  region_age_sex_population <- read_csv(path_region_age_sex_population)
+  
+  path_population_time <- paste0("Data/", country, "/", country, "_population_time.csv")
+  population_time <- read_csv(path_population_time)
+  
+  
+  return(list(age_sex_population, region_age_sex_population, population_time))
+            
+}
