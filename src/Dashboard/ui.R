639d34fffd262feaca2231a1ddc94d0b63a3bf3f
Maciek <sadkowskim@yahoo.pl>
2023-05-28 00:37:42 +0200

    added sidebar and buttons for UI

--- src/Dashboard/ui.R
+++ src/Dashboard/ui.R
@@ -8,26 +8,18 @@
 #
 
 library(shiny)
+library(shinydashboard)
 
 # Define UI for application that draws a histogram
-shinyUI(fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-            plotOutput("distPlot")
-        )
+shinyUI(
+    dashboardPage(
+        header <- dashboardHeader(title = "Demo Gauge"),
+        sidebar <- dashboardSidebar(sidebarMenu(menuItem("Population estimates", tabName = "popTab", icon = icon("dashboard")),
+                                                menuItem("Immigration estimates", tabName = "immigrantionTab", icon = icon("warning")),
+                                                menuItem("Model", tabName = "modelTab", icon = icon("book")))),
+        body <- dashboardBody(tabItems(
+            tabItem(tabName = "popTab", h2("Statystyki demograficzne kraju"),
+                    fluidRow())
+        ))
     )
-))
+)
